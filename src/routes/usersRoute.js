8c3e5da7a1690aab58e24562ad0eed5837781b4f
Mac <mac@Macs-MacBook-air.local>
2020-11-11 14:22:12 +0200

    confuguration of project login

--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -3,4 +3,4 @@ import userController from '../controllers/usersController';
 const router  = Router(); 
 
 router.post('/login', userController.login);
- export default router; 
+ export default rout
